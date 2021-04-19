c8c7bbdc49d13f379c4abf446b97e13de1f57fd9
kineticdirt <kineticdirt@gmail.com>
2021-04-19 01:23:20 -0700

    Commiting Multiplication

---
+++ Mult.hpp
@@ -0,0 +1,13 @@
+#include "base.hpp"
+#include <string>
+
+class Mult : public Base {
+        private:
+        	Base* value1;
+                Base* value2;
+        public:
+                Mult(Base* val1, Base* val2) : Base() { value1 = val1; value2 = val2;}
+                virtual double evaluate() { return (value1->evaluate()) * (value2->evaluate())};
+                virtual std::string stringify() { return "(" + value1->stringify() + "/" + value2->stringify() + ")"; }
+};
+
