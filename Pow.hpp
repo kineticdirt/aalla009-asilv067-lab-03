2c357530f2057b25add3695b847fe613a267687f
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 15:53:35 -0700

    Update Pow.hpp

--- Pow.hpp
+++ Pow.hpp
@@ -16,22 +16,7 @@ class Pow : public Base {
 		}
 
 		virtual double evaluate() { 
-			if(value2->evaluate() == 0.0){
-				return 1;
-			} else if((int)(value2->evaluate()) % 2 == 0){
-				return evaluate(value1->evaluate(), value2->evaluate()/2) * evaluate(value1->evaluate(), value2->evaluate()/2);
-			} else {
-				return value1->evaluate() * evaluate(value1->evaluate(), value2->evaluate()/2) * evaluate(value1->evaluate(), value2->evaluate()/2);
-			}
-		}
-		double evaluate(double val1, double val2){
-			if(val2 == 0.0){
-				return 1;
-			} else if((int)(val2) % 2 == 0){
-				return evaluate(val1, val2/2) * evaluate(val1, val2/2);
-			} else {
-				return val1 * evaluate(val1, val2/2) * evaluate(val1, val2/2);
-			}
+			return pow(value1->evaluate(), value2->evaluate());	
 		}
 		virtual std::string stringify() {return "(" + value1->stringify() + "**" + value2->stringify() + ")";}//added the"; atthe end
 };
