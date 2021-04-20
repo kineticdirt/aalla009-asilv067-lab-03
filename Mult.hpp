650f7662dbe4b41c664c11beadbad6ca9d8c07f2
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-19 19:46:09 -0700

    Mistyped a character

--- Mult.hpp
+++ Mult.hpp
@@ -8,6 +8,6 @@ class Mult : public Base {
         public:
                 Mult(Base* val1, Base* val2) : Base() { value1 = val1; value2 = val2;}
                 virtual double evaluate() { return (value1->evaluate()) * (value2->evaluate())};
-                virtual std::string stringify() { return "(" + value1->stringify() + "/" + value2->stringify() + ")"; }
+                virtual std::string stringify() { return "(" + value1->stringify() + "*" + value2->stringify() + ")"; }
 };
 
