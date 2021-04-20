0ce10d0f1b0fa646c6fda2c39d77ecec6a218346
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 12:16:29 -0700

    Updated Pow
    ->evaluate() added to values. Accessing address instead of value. Fixed the issue(probably)

--- Pow.hpp
+++ Pow.hpp
@@ -18,9 +18,9 @@ class Pow : public Base {
 			if(value2 == 0){
 				return 1;
 			} else if(value2 % 2==0){
-				return evaluate(value1, value2/2) * evaluate(value1, value2/2);
+				return evaluate(value1->evalutate(), (value2->evaluate())/2) * evaluate(value1_>evaluate(), (value2->evaluate())/2);
 			} else {
-				return value1 * evaluate(value1, value2/2) * evaluate(value1, value2/2);
+				return value1 * evaluate(value1->evaluate(), (value2->evaluate())/2) * evaluate(value1->evaluate(), (value2->evaluate())/2);
 			}
 		}
 		virtual std:string stringify() {return "(" + value1->stringify() + "**" + value2->stringify() + ")";}//added the"; atthe end
