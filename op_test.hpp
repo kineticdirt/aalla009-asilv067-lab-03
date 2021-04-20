c935451daf25a2396eab44ec20ca9d683744a097
Alexander Silva <72951915+asilv067@users.noreply.github.com>
2021-04-20 14:59:16 -0700

    Update op_test.hpp

--- op_test.hpp
+++ op_test.hpp
@@ -12,70 +12,70 @@
 #include "Rand.hpp"
 
 TEST(OpTest, OpEvaluateNonZero) {
-    Op* test = new Op(8);
+    Base* test = new Op(8);
     EXPECT_EQ(test->evaluate(), 8);
 }
 
 TEST(AddTest, AddEvaluatePositive) {  //add +	
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(4.0);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(4.0);
 	
-    Add* test = new Add(8.0, 4.0);
+    Add* test = new Add(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), 12.0);
 }
 
 TEST(AddTest, OpEvaluateNegative) { //add -
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(-4.0);
-    Add* test = new Add(8.0, -4.0);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(-4.0);
+    Add* test = new Add(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), 4.0);
 }
 
 TEST(SubTest, SubEvaluatePositive) { //sub +
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(4.0);
-    Sub* test = new Sub(8.0, 4.0);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(4.0);
+    Sub* test = new Sub(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), 4.0);
 }
 
 TEST(SubTest, SUbEvaluateNegative) { //sub -
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(3.0);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(3.0);
 	
-    Sub* test = new Sub(8.0, 3.0);
+    Sub* test = new Sub(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), 11.0);
 }
 
 TEST(MultTest, MultEvaluatePositive) { //Mult +
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(3.0);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(3.0);
 	
-    Mult* test = new Mult(8.0, 3.0);
+    Mult* test = new Mult(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), 24.0);
 }
 TEST(MultTest, MultEvaluateDecimal) { //Mult .5
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(0.5);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(0.5);
 	
-    Mult* test = new Mult(8.0, 0.5);
+    Mult* test = new Mult(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), 4.0);
 }
 TEST(DivTest, DivEvaluateNegative) { //Div -
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(-2.0);
-    Div* test = new Div(8.0, -2.0);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(-2.0);
+    Div* test = new Div(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), -4.0);
 }
 TEST(DivTest, DivEvaluateDecimal) { //Div .5
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(0.5);
-    Div* test = new Div(8.0, 0.5);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(0.5);
+    Div* test = new Div(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), 16.0);
 }
 TEST(PowTest, PowEvaluatePostive) { //Pow +
-	//Op* num1 = new Op(8.0);
-	//Op* num2 = new Op(3.0);
-    Pow* test = new Pow(8.0, 3.0);
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(3.0);
+    Pow* test = new Pow(num1, num2);
     EXPECT_DOUBLE_EQ(test->evaluate(), 64.0);
 }
 
