e25f0992793089ea4be2a5699d04c9fedf6a9346
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 14:59:42 -0700

    Update Sub.hpp

--- Sub.hpp
+++ Sub.hpp
@@ -6,12 +6,12 @@
 
 class Sub : public Base {
 	private:
-		double value1;
-		double value2;
+		Base* value1;
+		Base* value2;
 	public:
-		Sub(double val1, double val2) : Base() { value1 = val1; value2 = val2;}
+		Sub(Base* val1, Base* val2) : Base() { value1 = val1; value2 = val2;}
 
-		virtual double evaluate() { return value1 - value2; }
+		virtual double evaluate() { return value1->evaluate() - value2->evaluate(); }
 		virtual std::string stringify() { return "(" + value1->stringify() + "-" + value2->stringify() + ")"; }
 };
 
