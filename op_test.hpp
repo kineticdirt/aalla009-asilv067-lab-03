c220cdd3a7f631e50982eb4f609b2680e876127d
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 16:08:15 -0700

    Update op_test.hpp

--- op_test.hpp
+++ op_test.hpp
@@ -58,7 +58,7 @@ TEST(MultTest, MultEvaluateDecimal) { //Mult .5
 	Base* num2 = new Op(0.5);
 	
     Mult* test = new Mult(num1, num2);
-    EXPECT_DOUBLE_EQ(test->evaluate(), 4.0);
+    EXPECT_DOUBLE_EQ(test->stringify(), 4.0);
 }
 TEST(DivTest, DivEvaluateNegative) { //Div -
 	Base* num1 = new Op(8.0);
@@ -80,6 +80,79 @@ TEST(PowTest, PowEvaluatePostive) { //Pow +
 } 
 
 TEST(RandTest, RandEvaluateNonZero) {
+	Rand* test = new Rand();
+	EXPECT_TRUE( (test->evaluate() >= 0) && (test->evaluate() <= 100));
+}
+
+TEST(OpTest, OpEvaluateNonZero) {
+    Base* test = new Op(8);
+    EXPECT_EQ(test->evaluate(), 8);
+}
+
+TEST(AddTest, AddEvaluatePositive) {  //add + stringify	
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(4.0);
+	
+    Add* test = new Add(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), "(8.0+4.0)");
+}
+
+TEST(AddTest, OpEvaluateNegative) { //add - stringify
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(-4.0);
+    Add* test = new Add(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), "(8.0+-4.0)");
+}
+
+TEST(SubTest, SubEvaluatePositive) { //sub + stringify
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(4.0);
+    Sub* test = new Sub(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), "(8.0-4.0)");
+}
+
+TEST(SubTest, SUbEvaluateNegative) { //sub - stringify
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(-3.0);
+	
+    Sub* test = new Sub(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), "(8.0--3.0)");
+}
+
+TEST(MultTest, MultEvaluatePositive) { //Mult + stringify
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(3.0);
+	
+    Mult* test = new Mult(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), "(8.0*3.0)");
+}
+TEST(MultTest, MultEvaluateDecimal) { //Mult .5 stringify
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(0.5);
+	
+    Mult* test = new Mult(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), 4.0);
+}
+TEST(DivTest, DivEvaluateNegative) { //Div - stringify
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(-2.0);
+    Div* test = new Div(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), -4.0);
+}
+TEST(DivTest, DivEvaluateDecimal) { //Div .5 stringify
+	Base* num1 = new Op(8.0);
+	Base* num2 = new Op(0.5);
+    Div* test = new Div(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), 16.0);
+}
+TEST(PowTest, PowEvaluatePostive) { //Pow + stringify
+	Base* num1 = new Op(4.0);
+	Base* num2 = new Op(3.0);
+    Pow* test = new Pow(num1, num2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), 64.0);
+} 
+
+TEST(RandTest, RandEvaluateNonZero) { //stringify
 	Rand* test = new Rand();
 	EXPECT_TRUE( (test->evaluate() >= 0) && (test->evaluate() <= 100));
 }
