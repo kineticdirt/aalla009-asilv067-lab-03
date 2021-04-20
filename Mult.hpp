454bab08ae9f5706aa850cb529305d1e757c9018
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 14:35:18 -0700

    Update Mult.hpp

--- Mult.hpp
+++ Mult.hpp
@@ -6,10 +6,10 @@
 
 class Mult : public Base {
         private:
-        	double* value1;
-                double* value2;
+        	double value1;
+                double value2;
         public:
-                Mult(double* val1, double* val2) : Base() { value1 = val1; value2 = val2;}
+                Mult(double val1, double val2) : Base() { value1 = val1; value2 = val2;}
                 virtual double evaluate() { return (value1) * (value2);}
                 virtual std::string stringify() { return "(" + value1->stringify() + "*" + value2->stringify() + ")"; }
 };
