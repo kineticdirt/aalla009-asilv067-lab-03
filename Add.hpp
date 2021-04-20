df69dcf51ad68cd7252e86b7b95b82ab9121bca5
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 12:10:21 -0700

    Updated Add.hpp

--- Add.hpp
+++ Add.hpp
@@ -9,9 +9,19 @@ class Add : public Base {
                 Base* value1;
                 Base* value2;
         public:
-                Add(Base* val1, Base* val2) : Base() { value1 = val1; value2 = val2;
+                Add(Base* val1, Base* val2) : Base() 
+                { 
+                        value1 = val1; 
+                        value2 = val2;
+                }
 
-                virtual double evaluate() { return value1->evaluate() + value2->evaluate(); }
-                virtual std::string stringify() { return "(" + value1->stringify() + "+" + value2->stringify() + ")"; }
+                virtual double evaluate() 
+                { 
+                        return value1->evaluate() + value2->evaluate(); 
+                }
+                virtual std::string stringify() 
+                { 
+                        return "(" + value1->stringify() + "+" + value2->stringify() + ")"; 
+                }
 };
 
