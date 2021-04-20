0cca1b75cea67ff8631e49bad70fbb8a72f92e8c
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 14:25:20 -0700

    Update Div.hpp

--- Div.hpp
+++ Div.hpp
@@ -7,10 +7,10 @@
 
 class Div : public Base {
 	private:
-		Op* value1;
-		Op* value2;
+		Base* value1;
+		Base* value2;
 	public:
-		Div(Op* val1, Op* val2) : Base() { value1 = val1; value2 = val2;}
+		Div(Base* val1, Base* val2) : Base() { value1 = val1; value2 = val2;}
 		
 		virtual double evaluate() { return value1->evaluate() / value2->evaluate(); }
 		virtual std::string stringify() { return "(" + value1->stringify() + "/" + value2->stringify() + ")"; }
