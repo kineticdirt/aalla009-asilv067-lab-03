c8b76b7e078eace28ba8401c9bbb0fc91c93a8ff
Alexander Silva <72951915+asilv067@users.noreply.github.com>
2021-04-20 14:34:17 -0700

    Update op.hpp

--- op.hpp
+++ op.hpp
@@ -4,10 +4,14 @@
 #include "base.hpp"
 
 class Op : public Base {
-    public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+
+	private:
+		int value;
+	
+	public:
+        	Op(double value) : Base() { this->value = value; }
+        	virtual double evaluate() { return value; }
+        	virtual std::string stringify() { return std::to_string(value); }
 };
 
 #endif //__OP_HPP__
