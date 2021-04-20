1f33dd726a0c7046b07e9d8139c764929beee6e1
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 14:09:04 -0700

    Changing base to op

--- Div.hpp
+++ Div.hpp
@@ -1,15 +1,16 @@
 #ifndef __DIV_HPP__
 #define __DIV_HPP__
 
-#include "base.hpp"
+#include "op.hpp"
+#include "base.hpp
 #include <string>
 
 class Div : public Base {
 	private:
-		Base* value1;
-		Base* value2;
+		Op* value1;
+		Op* value2;
 	public:
-		Div(Base* val1, Base* val2) : Base() { value1 = val1; value2 = val2;}
+		Div(Op* val1, Op* val2) : Base() { value1 = val1; value2 = val2;}
 		
 		virtual double evaluate() { return value1->evaluate() / value2->evaluate(); }
 		virtual std::string stringify() { return "(" + value1->stringify() + "/" + value2->stringify() + ")"; }
