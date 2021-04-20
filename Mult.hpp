9cd854418c78919421e66667972da2f758dbe30b
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-04-20 14:09:56 -0700

    base to op

--- Mult.hpp
+++ Mult.hpp
@@ -1,14 +1,15 @@
 #ifndef __MULT_HPP__
 #define __MULT_HPP__
 #include "base.hpp"
+#include "op.hpp"
 #include <string>
 
 class Mult : public Base {
         private:
-        	Base* value1;
-                Base* value2;
+        	Op* value1;
+                Op* value2;
         public:
-                Mult(Base* val1, Base* val2) : Base() { value1 = val1; value2 = val2;}
+                Mult(Op* val1, Op* val2) : Base() { value1 = val1; value2 = val2;}
                 virtual double evaluate() { return (value1->evaluate()) * (value2->evaluate());}
                 virtual std::string stringify() { return "(" + value1->stringify() + "*" + value2->stringify() + ")"; }
 };
